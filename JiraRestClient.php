d46a66996fe50bcd62b310c212fcc8eebe3a1e03
rodger <rodger@ladela.com>
2012-06-29 12:21:04 +0300

    add needed methods

--- JiraRestClient.php
+++ JiraRestClient.php
@@ -22,98 +22,96 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-namespace Jira\Api;
 
-class RestClient{
-    protected $user;
-    protected $password;
-    
-    protected $endpoint;
-    
-    public function __construct($user, $password)
-    {
-        $this->user = $user;
-        $this->password = $password;
-    }
-    
-    public function getUserName()
-    {
-        return $this->user;
-    }
-    
-    public function getPassword()
-    {
-        return $this->password;
-    }
-    
-    public function getEndpoint()
-    {
-        return $this->endpoint;
-    }
-    
-    public function setEndPoint($url)
-    {
-        $this->endpoint = $url;
-    }
-    
-    public function getIssue($issueKey)
-    {
-        return $this->api("GET","/rest/api/2/issue/{$issueKey}");
-    }
-    
-    public function getIssueTypes()
-    {
-        return $this->api("GET","/rest/api/2/issuetype");
-    }
-    
-    public function createIssue($projectId, $summary, $description, $issueType, $options = array())
-    {
-        $default = array(
-            "project" => array(
-                "id"  => $projectId,
-            ),
-            "summary"     => $summary,
-            "description" => $description,
-            "issuetype"   => array(
-                "id" => $issueType,
-        ));
-        $default = array_merge($default, $options);
-        $result = $this->api("POST","/rest/api/2/issue/",array(
-            "fields" => $default
-        ));
-        /*
-            $result["id"];
-            $result["key"];
-            $result["self"];
-        */
-        return $result;
-    }
-    
-    public function api($method="GET", $url, $data = array())
-    {
-        $data = json_encode($data);
+class JiraRestClient
+{
+  protected $user;
+  protected $password;
 
-        $header = array(
-            "Authorization: Basic " . base64_encode($this->getUserName() . ":" . $this->getPassword()),
-        );
-        $header[] = 'Content-Type: application/json';
+  protected $endpoint;
 
-        if ($method=="POST") {
-            $header[] = 'Content-Length: ' . strlen($data);
-        }
-        
-        $context = array(
-            "http" => array(
-                "method"  => $method,
-                "header"  => join("\n", $header),
-        ));
-        $context['http']['content'] = $data;
-        
-        $data = file_get_contents($this->getEndpoint() . $url, 
-            false, 
-            stream_context_create($context)
-        );
-        
-        return json_decode($data,true);
+  public function __construct($endpoint)
+  {
+    $this->endpoint = $endpoint;
+  }
+
+  public function getUserName()
+  {
+    return $this->user;
+  }
+
+  public function setAuthentication($username, $password)
+  {
+    $this->user     = $username;
+    $this->password = $password;
+  }
+
+  public function getPassword()
+  {
+    return $this->password;
+  }
+
+  public function getEndpoint()
+  {
+    return $this->endpoint;
+  }
+
+  public function setEndPoint($url)
+  {
+    $this->endpoint = $url;
+  }
+
+  public function getIssue($issueKey)
+  {
+    return $this->api("GET", "/rest/api/2/issue/{$issueKey}");
+  }
+
+  public function getIssues($project_key)
+  {
+    return $this->api("POST", "/rest/api/2/search", array('jql' => sprintf('assignee=currentUser() and project=%s', $project_key)));
+  }
+
+  public function getIssueTypes()
+  {
+    return $this->api("GET", "/rest/api/2/issuetype");
+  }
+
+  public function getProjects()
+  {
+    return $this->api('GET', '/rest/api/2/project');
+  }
+
+  public function login()
+  {
+    return $this->api('GET', '/rest/auth/latest/session');
+  }
+
+  public function api($method = "GET", $url, $data = array())
+  {
+    $data = json_encode($data);
+
+    $header   = array(
+      "Authorization: Basic " . base64_encode($this->getUserName() . ":" . $this->getPassword()),
+    );
+    $header[] = 'Content-Type: application/json';
+
+    if ($method == "POST") {
+      $header[] = 'Content-Length: ' . strlen($data);
     }
+
+    $context                    = array(
+      "http" => array(
+        "method"  => $method,
+        "header"  => implode("\n", $header),
+      ));
+    $context['http']['content'] = $data;
+
+    $data = file_get_contents($this->getEndpoint() . $url,
+      false,
+      stream_context_create($context)
+    );
+
+    return json_decode($data, true);
+  }
 }
+
